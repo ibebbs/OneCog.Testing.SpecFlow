c42fe82ae3d96863cd0d3f0bbc8d28005860ba98
ibebbs <github@bebbs.co.uk>
2014-09-23 15:15:49 +0100

    Initial commit
    Implemented basic extensions for reading generic types from table row
    columns

---
+++ OneCog.Testing.SpecFlow.Tests/TestExtensions.cs
@@ -0,0 +1,53 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+using OneCog.Testing.SpecFlow;
+
+namespace OneCog.Testing.SpecFlow.Tests
+{
+    [TestFixture]
+    public class TestExtensions
+    {
+        public enum Gender { Male, Female }
+
+        [Test]
+        public void ShouldBeAbleToReadAString()
+        {
+            Table table = new Table(new[] { "Name", "Age", "Gender", "Height" });
+            table.AddRow(new[] { "Ian", "37", "Male", "1.73" });
+
+            Assert.That(table.Rows.First().Read<string>("Name"), Is.EqualTo("Ian"));
+        }
+
+        [Test]
+        public void ShouldBeAbleToReadAnInteger()
+        {
+            Table table = new Table(new[] { "Name", "Age", "Gender", "Height" });
+            table.AddRow(new[] { "Ian", "37", "Male", "1.73" });
+
+            Assert.That(table.Rows.First().Read<int>("Age"), Is.EqualTo(37));
+        }
+
+        [Test]
+        public void ShouldBeAbleToReadADouble()
+        {
+            Table table = new Table(new[] { "Name", "Age", "Gender", "Height" });
+            table.AddRow(new[] { "Ian", "37", "Male", "1.73" });
+
+            Assert.That(table.Rows.First().Read<double>("Height"), Is.EqualTo(1.73));
+        }
+
+        [Test]
+        public void ShouldBeAbleToReadAnEnumeration()
+        {
+            Table table = new Table(new[] { "Name", "Age", "Gender", "Height" });
+            table.AddRow(new[] { "Ian", "37", "Male", "1.73" });
+
+            Assert.That(table.Rows.First().ReadEnum<Gender>("Gender"), Is.EqualTo(Gender.Male));
+        }
+    }
+}
---
+++ OneCog.Testing.SpecFlow/Extensions.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.ComponentModel;
+using System.Globalization;
+using TechTalk.SpecFlow;
+
+namespace OneCog.Testing.SpecFlow
+{
+    public static class Extensions
+    {
+        public static T Read<T>(this TableRow row, string column)
+        {
+            TypeConverter converter = TypeDescriptor.GetConverter(typeof(T));
+            string value;
+
+            if (row.TryGetValue(column, out value))
+            {
+                return (T)converter.ConvertFromString(null, CultureInfo.InvariantCulture, value);
+            }
+            else
+            {
+                throw new ArgumentException(string.Format("A column named '{0}' does not exist in the table", column), "column");
+            }
+        }
+
+        public static T ReadOrDefault<T>(this TableRow row, string column)
+        {
+            TypeConverter converter = TypeDescriptor.GetConverter(typeof(T));
+            string value;
+
+            if (row.TryGetValue(column, out value))
+            {
+                return (T)converter.ConvertFromString(null, CultureInfo.InvariantCulture, value);
+            }
+            else
+            {
+                return default(T);
+            }
+        }
+
+        public static T ReadEnum<T>(this TableRow row, string column) where T : struct
+        {
+            string value;
+            T result;
+
+            if (row.TryGetValue(column, out value))
+            {
+                if (Enum.TryParse<T>(value, out result))
+                {
+                    return result;
+                }
+                else
+                {
+                    throw new InvalidOperationException(string.Format("'{0}' is not a valid value for the enumeration '{1}", value, typeof(T).Name));
+                }
+            }
+            else
+            {
+                throw new ArgumentException(string.Format("A column named '{0}' does not exist in the table", column), "column");
+            }            
+        }
+    }
+}
